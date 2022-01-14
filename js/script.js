d7476f43a724afd4a9b33325773caa3ccd0172a0
Knot <akthakorn@gmail.com>
2022-01-14 23:08:43 +0700

    update

--- js/script.js
+++ js/script.js
@@ -1,12 +1,40 @@
-var number = prompt("Please Number")
+let point = 0
+let check = false
+let h = String("หัว");
+let g = String("ก้อย");
 
-document.getElementById("num").innerHTML = number
-var rand = Math.floor(Math.random() * 2)
-document.getElementById("result").innerHTML = rand
+var cnt = prompt("Enter Rounds")
+for (var i = 1; i <= cnt; i++) {
 
-if (rand == number) {
-    alert("ยินดีด้วยคุณถูกรางวัล")
-    document.getElementById("out").innerHTML = "ยินดีด้วยคุณถูกรางวัล"
-} else{
-    document.getElementById("out").innerHTML = "คุณยังไม่ถูกรางวัล"
+    check = false
+    do {
+        var ans = prompt("หัว หรือ ก้อย ทายมาเลย")
+        let resulth = ans.localeCompare(h)
+        let resultg = ans.localeCompare(g)
+
+        if (resultg == 0 || resulth == 0) {
+            check = true
+        } else {
+            alert("กรุณาใส่ข้อความ หัว หรือ ก้อย ให้ถูกนะ!")
+        }
+    } while (!check)
+
+    let rand_ans = ""
+
+    if (Math.floor(Math.random() * 2) == 0) {
+        rand_ans = "หัว"
+    } else {
+        rand_ans = "ก้อย"
+    }
+
+    if (ans == rand_ans) {
+        point = point + 1
+        document.getElementById("game-list").innerHTML += "รอบที่: " + i + " ตอบ: " + ans + "<br>" + "คำตอบคือ: " + rand_ans + "<br>"
+        alert("ตอบถูกเก่งนะเนี้ย!")
+    } else {
+        document.getElementById("game-list").innerHTML += "รอบที่: " + i + " ตอบ: " + ans + "<br>" + "คำตอบคือ: " + rand_ans + "<br>"
+        alert("อ้าวตอบผิดซะงั้น!")
+    }
 }
+
+document.getElementById("game-list").innerHTML += "<br>" + "รวมคะแนนที่ได้: " + point
